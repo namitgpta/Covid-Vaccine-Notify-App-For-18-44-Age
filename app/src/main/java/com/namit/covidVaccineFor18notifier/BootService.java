b7f836834dd8b5da9a6cb9bd22dbf54feba60ebc
namitgpta <namitg677@gmail.com>
2021-05-08 00:18:26 +0530

    2.0 Version Release - Huge improvements+++

--- app/src/main/java/com/namit/covidVaccineFor18notifier/BootService.java
+++ app/src/main/java/com/namit/covidVaccineFor18notifier/BootService.java
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.util.Log;
 import android.widget.Toast;
 
 public class BootService extends BroadcastReceiver {
@@ -12,7 +13,7 @@ public class BootService extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         if(intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)){
             //code to run after boot
-            Toast.makeText(context, "Boot completed!", Toast.LENGTH_SHORT).show();
+            Log.d("Vaccine", "Boot completed!");
 
             SharedPreferences prefs = context.getSharedPreferences("com.namit.vaccine18notifier", Context.MODE_PRIVATE);
             String pincode_here = prefs.getString("stored_pincode", "");
